01fd571178960690be781f257cb2cbd6d1f1707e
Paul Schultz <pschultz@pobox.com>
2022-01-13 13:59:09 -0600

    Mosaic Issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next';
+import { JsonSchemaViewer } from '@stoplight/json-schema-viewer';
+import { schema } from '../devfileJsonSchema';
 
 export const Home: NextPage = () => (
   <div>
-    <p>Hello World!</p>
+    <JsonSchemaViewer schema={schema} emptyText="No schema defined" defaultExpandedDepth={0} />
   </div>
 );
 
