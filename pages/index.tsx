ba4a540246962e80ee83be4733a11586d401707e
Paul Schultz <pschultz@pobox.com>
2022-06-10 15:53:17 -0500

    schema viewer demo
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next';
+import { Box, Provider as MosaicProvider, injectStyles } from '@stoplight/mosaic';
+import { JsonSchemaViewer } from '@stoplight/json-schema-viewer';
+import { devfileSchema } from '../devfileJsonSchema';
 
-export const Home: NextPage = () => (
-  <div>
-    <p>Hello World!</p>
-  </div>
-);
+export const Home: NextPage = () => {
+  injectStyles();
+
+  return (
+    <MosaicProvider>
+      <Box mx="auto" py={20} px={8} style={{ maxWidth: 800 }}>
+        <JsonSchemaViewer
+          schema={devfileSchema}
+          emptyText="No schema defined"
+          renderRootTreeLines
+        />
+      </Box>
+    </MosaicProvider>
+  );
+};
 
 export default Home;
--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 
 export interface MetaProps {
   title?: string;
@@ -7,21 +8,28 @@ export interface MetaProps {
 }
 
 export const Meta: React.FC<MetaProps> = ({ title, keywords, description }: MetaProps) => (
-  <Head>
-    <title>{title}</title>
-    <meta name="keywords" content={keywords} />
-    <meta name="description" content={description} />
-
-    <meta charSet="utf8" />
-    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-    <meta
-      name="viewport"
-      content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
+  <>
+    <Script
+      src="https://kit.fontawesome.com/1e1f25269b.js"
+      crossOrigin="anonymous"
+      data-auto-replace-svg="nest"
     />
-    <meta name="theme-color" content="#151515" />
+    <Head>
+      <title>{title}</title>
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
 
-    <link rel="icon" href="/images/icons/favicon.ico" />
-  </Head>
+      <meta charSet="utf8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
+      />
+      <meta name="theme-color" content="#151515" />
+
+      <link rel="icon" href="/images/icons/favicon.ico" />
+    </Head>
+  </>
 );
 Meta.displayName = 'Meta';
 
